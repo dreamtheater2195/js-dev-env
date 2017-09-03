4361ea94409058ad8a72fc708211adfd7fe44f4c
Quan Minh Pham <dreamtheater2195@gmail.com>
2017-09-03 17:13:44 +0700

    Finish module 11-MockHTTP

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -14,8 +14,18 @@ app.use(require('webpack-dev-middleware')(compiler, {
   noInfo: true,
   publicPath: config.output.publicPath
 }));
+
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
+});
+
+app.get('/users', function(req, res) {
+  //hard coding for simplicity
+  res.json([
+    {"id": 1, "firstName": "Bob", "lastName":"Smith", "email":"bob@gmail.com"},
+    {"id": 2, "firstName": "Tammy", "lastName":"Norton", "email":"tammy@gmail.com"},
+    {"id": 3, "firstName": "Tina", "lastName":"Lee", "email":"lee.tina@gmail.com"}
+  ]);
 });
 
 app.listen(port, function(err){
